d53b4d964c3a05e8546b6d38f085906ddc53ab55
muehlemann <mattm@honeycommb.com>
2023-01-12 16:44:59 +0100

    Main page and MTG counter updates

--- js/index.js
+++ js/index.js
@@ -5,14 +5,39 @@ var albums = [
 		"cover": "tame-impala-currents.png"
 	},
 	{
-		"artist": "Pink Floyd",
-		"title": "Wish You Were Here",
-		"cover": "pink-floyd-wish-you-were-here.png"
+		"artist": "L'Imp&eacute;ratrice",
+		"title": "Odyss&eacute;e",
+		"cover": "limperatrice-odyssee.jpg"
 	},
 	{
-		"artist": "Pink Floyd",
-		"title": "The Dark Side of the Moon",
-		"cover": "pink-floyd-the-dark-side-of-the-moon.png"
+		"artist": "Parcels",
+		"title": "Live Vol. 1",
+		"cover": "parcels-live-vol-1.jpeg"
+	},
+	{
+		"artist": "Arctic Monkeys",
+		"title": "Tranquility Base Hotel &amp;...",
+		"cover": "arctic-monkeys-tranquility-base-hotel-and-casino.png",
+	},
+	{
+		"artist": "Toro y Moi",
+		"title": "MAHAL",
+		"cover": "toro-y-moi-mahal.png"
+	},
+	{
+		"artist": "Lime Cordiale",
+		"title": "Permanent Vacation",
+		"cover": "lime-cordiale-permanent-vacation.png"
+	},
+	{
+		"artist": "Kikagaku Moyo",
+		"title": "Kumoyo Island",
+		"cover": "kikagaku-moyo-kumoyo-island.png"
+	},
+	{
+		"artist": "Mac Miller",
+		"title": "Circles",
+		"cover": "mac-miller-circles.png"
 	},
 	{
 		"artist": "Logic",
@@ -23,6 +48,16 @@ var albums = [
 		"artist": "Logic",
 		"title": "The Incredible True Story",
 		"cover": "logic-thei-ncredible-true-story.jpg"
+	},
+	{
+		"artist": "Pink Floyd",
+		"title": "Wish You Were Here",
+		"cover": "pink-floyd-wish-you-were-here.png"
+	},
+	{
+		"artist": "Pink Floyd",
+		"title": "The Dark Side of the Moon",
+		"cover": "pink-floyd-the-dark-side-of-the-moon.png"
 	},
 	{
 		"artist": "Led Zeppelin",
@@ -43,17 +78,17 @@ var albums = [
 		"artist": "The Beatles",
 		"title": "Abbey Road",
 		"cover": "beatles-abbey-road.jpg"
-	},
+	}
 ];
 
 function docReady(fn) {
-    // see if DOM is already available
-    if (document.readyState === "complete" || document.readyState === "interactive") {
-        // call on next available tick
-        setTimeout(fn, 1);
-    } else {
-        document.addEventListener("DOMContentLoaded", fn);
-    }
+	// see if DOM is already available
+	if (document.readyState === "complete" || document.readyState === "interactive") {
+			// call on next available tick
+			setTimeout(fn, 1);
+	} else {
+		document.addEventListener("DOMContentLoaded", fn);
+	}
 }
 
 docReady(function() {
